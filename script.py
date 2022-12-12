f7b7f80184ec62633cafbd42453b2922bc3d1fda
Michael <managedkaos@users.noreply.github.com>
2022-12-12 08:39:05 -0800

    Update script.py

--- script.py
+++ script.py
@@ -14,19 +14,22 @@ ydl_opts = {
 urls = bios.read('urls.yaml')
 
 for video_url in urls:
-    print(video_url)
 
     video_id = video_url.split('=')[1]
 
     with youtube_dl.YoutubeDL(ydl_opts) as ydl:
         ydl.download([video_url])
 
+    # TODO: Update the script to use in memory data vs reading the video details from a local file
     video = bios.read(f"{video_id}.info.json")
 
     print(f"## {video['upload_date']} - {video['title']}")
+    print(f"   {video_url}")
 
     if 'chapters' in video:
         for chapter in video['chapters']:
             print(f"- {chapter['title']}")
     else:
         print('No chapters found for video.')
+    
+    print();
